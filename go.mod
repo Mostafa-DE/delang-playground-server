4eca074134dcf0711446764180ee27a2c38bdbc7
mostafa-de <mostafafayyado1@gmail.com>
2023-12-08 18:43:04 +0300

    initial commit :)

---
+++ go.mod
@@ -0,0 +1,11 @@
+module server
+
+go 1.18
+
+require (
+	github.com/Mostafa-DE/delang v0.0.6
+	github.com/gomarkdown/markdown v0.0.0-20231115200524-a660076da3fd
+	github.com/google/uuid v1.4.0
+)
+
+require github.com/shopspring/decimal v1.3.1 // indirect
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+)
+
+func main() {
+	port := getPort()
+
+	initAppRoutes()
+
+	fmt.Printf("Server started on port %s\n", port)
+
+	go func() {
+		fmt.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), enableCORS(http.DefaultServeMux))
+
+	if err != nil {
+		fmt.Printf("Something went wrong while starting the server: %s", err)
+	}
+}
